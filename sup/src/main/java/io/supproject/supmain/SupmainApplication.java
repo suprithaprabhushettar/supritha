b8e3117a5ed0383edb748c17a20cbe69483d5b84
supritha <spprabhushettar@gmail.com>
2020-03-30 19:21:18 +0530

    first one

---
+++ sup/src/main/java/io/supproject/supmain/SupmainApplication.java
@@ -0,0 +1,13 @@
+package io.supproject.supmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupmainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupmainApplication.class, args);
+    }
+
+}
